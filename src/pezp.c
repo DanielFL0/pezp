d4f7e0a589e08beca15f3ae952631af7d71f75ee
Daniel彡 <jose.saldanafrs@uanl.edu.mx>
2023-03-10 15:27:38 -0600

    Update pezp.c

--- src/pezp.c
+++ src/pezp.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "node.h"
 #include "linked_list.h"
+#include "arena.h"
 
 int main(void) {
   linked_list* list = create_linked_list();
   insert(list, 1);
   insert(list, 2);
-  insert(list, 3);
   print_linked_list(list);
+  delete_linked_list(list);
+
   return 0;
 }
