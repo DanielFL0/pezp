a434d33bb3cc838b7796bda1cf699d0c74a33e39
Daniel彡 <jose.saldanafrs@uanl.edu.mx>
2023-04-25 17:54:52 -0500

    Update pezp.c

--- src/pezp.c
+++ src/pezp.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "node.h"
 #include "linked_list.h"
-#include "arena.h"
+#include "defs.h"
+#include "virtual_machine.h"
 
 int main(void) {
-  linked_list* list = create_linked_list();
-  insert(list, 1);
-  insert(list, 2);
-  print_linked_list(list);
-  delete_linked_list(list);
-
+  virtual_machine* vm = create_virtual_machine();
+  run(vm);
   return 0;
 }
