7dec5a46f67e6e5a7d0e8d8d799d44f21cfd7092
Daniel彡 <jose.saldanafrs@uanl.edu.mx>
2023-04-26 16:02:08 -0500

    Update pezp.c

--- src/pezp.c
+++ src/pezp.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdint.h>
+#include "chunk.h"
 #include "node.h"
 #include "linked_list.h"
 #include "defs.h"
-#include "eval.h"
+#include "file_loader.h"
+#include "virtual_machine.h"
 
 /*
  * PROCEDURE:
@@ -14,19 +16,15 @@
  * Go back to step 1.
  */
 
-int ip = 0; /* instruction pointer */
-int sp = 0; /* stack pointer */
-
 int main(void) {
   linked_list* memory = create_linked_list();
-  for (int i = 0; i < 10; i++)
-    insert_node(memory, i);
+  for (int i = 0; i < 10; i++) {
+    chunk_t* chunk = create_chunk(i, i);
+    insert_node(memory, chunk);
+  }
   print_linked_list(memory);
   node_t* test = get_node(memory, 2);
-  set_node(test, 44);
-  print_linked_list(memory);
-  delete_node(test);
-  print_linked_list(memory);
+  print_node(test);
   delete_linked_list(memory);
   return 0;
 }
