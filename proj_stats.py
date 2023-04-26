31b0855ce373c6592b44c0f5cf824141ec83d333
Daniel彡 <jose.saldanafrs@uanl.edu.mx>
2023-04-26 12:07:25 -0500

    Update proj_stats.py

--- proj_stats.py
+++ proj_stats.py
@@ -1,22 +1,23 @@
 from os import getcwd, listdir, path
 
 cwd = getcwd()
+src_dir = path.join(cwd, 'src')
 
-def stats(filepath):
+def print_file_stats(filepath):
     line_count = 0
     word_count = 0
     with open(filepath, 'r') as file_object:
         lines = file_object.readlines()
         for line in lines:
-            word_count = len(line.split(' '))
+            word_count += len(line.split(' '))
             line_count += 1
-    print(f"FILENAME:\t{filepath}\nWORDS:\t{word_count}\nLINES:\t{line_count}")
+    print(f"{filepath}\nWORDS:\t{word_count}\nLINES:\t{line_count}")
 
 
-def recursive_check(dirpath):
-    dirs = listdir(dirpath)
-    for item in dirs:
-        if path.isdir(item):
-            recursive_check(item)
-        print(item)
-recursive_check(cwd)
+def get_file_stats(dirpath):
+    files = listdir(dirpath)
+    for filename in files:
+        filepath = path.join(dirpath, filename)
+        print_file_stats(filepath)
+
+get_file_stats(src_dir)
