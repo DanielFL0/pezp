7a52aec23bfd38366d34cf0af39a8e790012bde2
Daniel彡 <jose.saldanafrs@uanl.edu.mx>
2023-04-26 11:28:47 -0500

    Update pezp.c

--- src/pezp.c
+++ src/pezp.c
@@ -4,10 +4,19 @@
 #include "node.h"
 #include "linked_list.h"
 #include "defs.h"
-#include "virtual_machine.h"
+#include "eval.h"
+
+int ip = 0; /* instruction pointer */
+int sp = 0; /* stack pointer */
 
 int main(void) {
-  virtual_machine* vm = create_virtual_machine();
-  run(vm);
+  linked_list* memory = create_linked_list();
+  for (int i = 0; i < 100; i++)
+    insert_node(memory, i);
+  print_linked_list(memory);
+  node_t* test = get_node(memory, 2);
+  test->vertex = 44;
+  print_linked_list(memory);
+  delete_linked_list(memory);
   return 0;
 }
