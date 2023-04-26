ff925ff30910773ee4a68ebd81e29efd9bf16093
Daniel彡 <jose.saldanafrs@uanl.edu.mx>
2023-04-26 11:28:43 -0500

    Update node.h

--- src/node.h
+++ src/node.h
@@ -4,9 +4,12 @@
 typedef struct node_t {
   int             vertex;
   struct node_t*  next;
+  struct node_t*  prev;
 } node_t;
 
 node_t* create_node(int vertex);
+node_t* next_node(node_t* node);
+node_t* prev_node(node_t* node);
 void delete_node(node_t* node);
 void print_node(node_t* node);
 
