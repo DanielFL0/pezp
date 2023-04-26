b1d57008f6e9ef180c9bd59fac62bfed343f8887
Daniel彡 <jose.saldanafrs@uanl.edu.mx>
2023-04-26 11:32:50 -0500

    Update pezp.c

--- src/pezp.c
+++ src/pezp.c
@@ -11,11 +11,11 @@ int sp = 0; /* stack pointer */
 
 int main(void) {
   linked_list* memory = create_linked_list();
-  for (int i = 0; i < 100; i++)
+  for (int i = 0; i < 10; i++)
     insert_node(memory, i);
   print_linked_list(memory);
   node_t* test = get_node(memory, 2);
-  test->vertex = 44;
+  set_node(test, 44);
   print_linked_list(memory);
   delete_linked_list(memory);
   return 0;
