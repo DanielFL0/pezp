2c5fa9e4814d6bf4d7abc01ec4316098a13848b2
Daniel彡 <jose.saldanafrs@uanl.edu.mx>
2023-05-18 22:56:04 -0500

    Update chunk.h

--- src/chunk.h
+++ src/chunk.h
@@ -1,18 +1,17 @@
 #ifndef CHUNK_H
 #define CHUNK_H
 
-#define IS_CHUNK_VALID(chunk) ((chunk)->count > 0)
+#define MEMORY_MAX (1 << 8)
 
 typedef struct chunk_t {
-  uint32_t  count;
-  uint32_t  capacity;
-  uint8_t*  code;
+  uint8_t*  buffer;
+  uint8_t*  buffer_offset;
+  uint8_t*  buffer_length;
 } chunk_t;
 
 void init_chunk(chunk_t* chunk);
-void read_chunk(chunk_t* chunk);
 void write_chunk(chunk_t* chunk, uint8_t byte);
+void disassemble_chunk(chunk_t* chunk, char* name);
 void free_chunk(chunk_t* chunk);
-
 
 #endif
