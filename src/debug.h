9f9a0c1a597d303480cb9a2fbfa4ff1d8649dc26
Daniel彡 <jose.saldanafrs@uanl.edu.mx>
2023-05-29 20:38:23 -0500

    full project rewrite
    Bad design choices, starting up from scratch

--- src/debug.h
+++
@@ -1,8 +0,0 @@
-#ifndef DEBUG_H
-#define DEBUG_H
-
-void disassemble_chunk(chunk_t* chunk, const char* name);
-int disassemble_instruction(chunk_t* chunk, int offset);
-void simple_instruction(const char* name);
-
-#endif
