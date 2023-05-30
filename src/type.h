f69a19c4e22261b0611f48e6e48408c714271fe7
Daniel彡 <jose.saldanafrs@uanl.edu.mx>
2023-05-29 21:08:31 -0500

    Create type.h

---
+++ src/type.h
@@ -0,0 +1,15 @@
+#ifndef TYPE_H
+#define TYPE_H
+
+typedef enum type_T {
+  INT8_T    = 0x00,
+  INT16_T   = 0x01,
+  INT32_T   = 0x02,
+  UINT8_T   = 0x03,
+  UINT16_T  = 0x04,
+  UINT32_T  = 0x05,
+  FLOAT32_T = 0x06,
+  FLOAT64_T = 0x07,
+} type_t;
+
+#endif
